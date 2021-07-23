3735a929cf51fb4e1be30e0e66b8aca587df2cde
robinNcode <msmrobin518@gmail.com>
2021-07-24 02:33:22 +0600

    mysql connection done

--- app/views.py
+++ app/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     context = {
-        'name' : 'MD Shahin Mia Robin'
+        'name': 'MD Shahin Mia Robin',
+        'list1': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     }
     return render(request, 'index.html', context)
     # return HttpResponse("This is homepage")
--- firstWeb/settings.py
+++ firstWeb/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'firstWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'robin',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
